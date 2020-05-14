07385a39dd881cc87c8f8c17fb0626a40517f5e3
Alex-0293 <1928311@tuta.io>
2020-05-14 16:00:50 +0300

    Add statistic.

--- SCRIPTS/ConsultantPlusUtilities.ps1
+++ SCRIPTS/ConsultantPlusUtilities.ps1
@@ -18,9 +18,10 @@ Param (
     [datetime] $LogCutDate
 )
 Clear-Host
-$Global:ScriptName = $MyInvocation.MyCommand.Name
+$Global:ScriptInvocation = $MyInvocation
 $InitScript        = "C:\DATA\Projects\GlobalSettings\SCRIPTS\Init.ps1"
-if (. "$InitScript" -MyScriptRoot (Split-Path $PSCommandPath -Parent) -force ) { exit 1 }
+. "$InitScript" -MyScriptRoot (Split-Path $PSCommandPath -Parent)
+if ($LastExitCode) { exit 1 }
 
 # Error trap
 trap {
@@ -29,7 +30,7 @@ trap {
         . "$GlobalSettings\$SCRIPTSFolder\Finish.ps1" 
     }
     Else {
-        Write-Host "There is error before logging initialized." -ForegroundColor Red
+        Write-Host "[$($MyInvocation.MyCommand.path)] There is error before logging initialized." -ForegroundColor Red
     }   
     exit 1
 }
@@ -41,66 +42,83 @@ switch ($Service.ToUpper()) {
     "UPDATE"    { 
         $ScriptBlock = {
             $Res = [PSCustomObject]@{
-                Data      = $Null
-                LogBuffer = $Null
+                Data               = $Null
+                LogBuffer          = @{}
+                FreeDiskSpace      = 0
+                NewFreeDiskSpace   = 0
             }
 
-            $ConsPath          = $Using:ConsPath
-            $UpdateArguments   = $Using:UpdateArguments
-            $WorkDir           = $Using:WorkDir
-            $ScriptLogFilePath = $Using:ScriptLogFilePath
-
+            $ConsPath            = $Using:ConsPath
+            $UpdateArguments     = $Using:UpdateArguments
+            $WorkDir             = $Using:WorkDir
+            $ScriptLogFilePath   = $Using:ScriptLogFilePath
+            $Res.FreeDiskSpace   = [math]::Round((Get-PSDrive ((Get-Item $WorkDir).PSDrive.Name)).Free / 1gb, 2)
+            
             $Process = Start-Program  -Program $ConsPath -LogFilePath $ScriptLogFilePath -Arguments $UpdateArguments -WorkDir $WorkDir -Wait -Evaluate
 
-            $Res.Data      = $Process
-            $Res.LogBuffer = $Global:LogBuffer
+            $Res.Data               = $Process
+            $Res.LogBuffer          = $Global:LogBuffer
+            $Res.NewFreeDiskSpace   = [math]::Round((Get-PSDrive ((Get-Item $WorkDir).PSDrive.Name)).Free / 1gb, 2)
 
             return  $Res
         }
         Add-ToLog -Message "Starting update." -logFilePath $ScriptLogFilePath -Display -Status "Info" -Level ($ParentLevel + 1)
         $Res = Invoke-PSScriptBlock -ScriptBlock $Scriptblock -Computer $RemoteComputer -ImportLocalModule "AlexkUtils"
-        if ($res) {
+        if (@($res.LogBuffer).Count) {
             foreach ($item in $Res.LogBuffer) {
                 Add-ToLog @item
-            }
+            }            
         }
-        . "$PSCommandPath\$ScriptName" -LogCutDate $Global:ScriptStartTime
+        if ($Res.NewFreeDiskSpace -and $Res.FreeDiskSpace) {
+            Add-ToLog -Message "Free disk [$($ConsPath.Substring(0, 1)):] space changed on [$RemoteComputer] from [$($Res.FreeDiskSpace) GB] to [$($Res.NewFreeDiskSpace) GB], difference [$([math]::round(($Res.NewFreeDiskSpace - $Res.FreeDiskSpace),2)) GB]" -logFilePath $ScriptLogFilePath -Display -Status "Info" -Level ($ParentLevel + 1)
+        }
+
+        . "$PSCommandPath" -LogCutDate $Global:ScriptStartTime
     }
     "SHRINK" { 
         $ScriptBlock = {
             $Res = [PSCustomObject]@{
-                Data      = $Null
-                LogBuffer = $Null
+                Data             = $Null
+                LogBuffer        = @{}
+                FreeDiskSpace    = 0
+                NewFreeDiskSpace = 0
             }
 
-            $ConsPath          = $Using:ConsPath
-            $ShrinkArguments   = $Using:ShrinkArguments
-            $WorkDir           = $Using:WorkDir
-            $ScriptLogFilePath = $Using:ScriptLogFilePath
+            $ConsPath            = $Using:ConsPath
+            $ShrinkArguments     = $Using:ShrinkArguments
+            $WorkDir             = $Using:WorkDir
+            $ScriptLogFilePath   = $Using:ScriptLogFilePath
+            $Res.FreeDiskSpace   = [math]::Round((Get-PSDrive ((Get-Item $WorkDir).PSDrive.Name)).Free / 1gb, 2)
 
             $Process = Start-Program  -Program $ConsPath -LogFilePath $ScriptLogFilePath -Arguments $ShrinkArguments -WorkDir $WorkDir -Wait -Evaluate
 
-            $Res.Data      = $Process
-            $Res.LogBuffer = $Global:LogBuffer
+            $Res.Data               = $Process
+            $Res.LogBuffer          = $Global:LogBuffer
+            $Res.NewFreeDiskSpace   = [math]::Round((Get-PSDrive ((Get-Item $WorkDir).PSDrive.Name)).Free / 1gb, 2)
 
             return  $Res
         }
         Add-ToLog -Message "Starting database shrink." -logFilePath $ScriptLogFilePath -Display -Status "Info" -Level ($ParentLevel + 1)
         $Res = Invoke-PSScriptBlock -ScriptBlock $Scriptblock -Computer $RemoteComputer -ImportLocalModule "AlexkUtils"
-        if ($res) {
+        if (@($res.LogBuffer).Count) {
             foreach ($item in $Res.LogBuffer) {
-                Add-ToLog @item
-            }
+                Add-ToLog @item   
+            }            
         }
-        . "$PSCommandPath\$ScriptName" -LogCutDate $Global:ScriptStartTime
+        if ($Res.NewFreeDiskSpace -and $Res.FreeDiskSpace) {
+            Add-ToLog -Message "Free disk [$($ConsPath.Substring(0, 1)):] space changed on [$RemoteComputer] from [$($Res.FreeDiskSpace) GB] to [$($Res.NewFreeDiskSpace) GB], difference [$([math]::round(($Res.NewFreeDiskSpace - $Res.FreeDiskSpace),2)) GB]" -logFilePath $ScriptLogFilePath -Display -Status "Info" -Level ($ParentLevel + 1)
+        }
+
+        . "$PSCommandPath" -LogCutDate $Global:ScriptStartTime
     }
     Default {
         $Scriptblock = {
             $Res = [PSCustomObject]@{
-                ConsErrorFile    = $Null
-                ConsInetFileList = $Null
-                ConsInetFile     = $Null
-                LogBuffer        = $Null
+                ConsErrorFile      = ""
+                ConsInetFileList   = ""
+                ConsInetFile       = ""
+                LogBuffer          = @{}
+                AvgDlSpeed         = ""
             }
 
             $LogCutDate         = $using:LogCutDate
@@ -110,49 +128,7 @@ switch ($Service.ToUpper()) {
             $ConsErrorFile      = $using:ConsErrorFile
             $ConsInetFileList   = $using:ConsInetFileList
             $ConsInetFile       = $using:ConsInetFile
-            $ParentLevel        = $using:ParentLevel
-            Function Convert-InetFileListContentToCSV ($TodayInetFileListContent) {
-                $SpeedStat = $TodayInetFileListContent | Select-Object -last 1 
-                $TodayInetFileListContent = $TodayInetFileListContent | Select-Object -last ($TodayInetFileListContent.count - 2) 
-                $TodayInetFileListContent = $TodayInetFileListContent | Select-Object -first ($TodayInetFileListContent.count - 1) 
-                $TodayInetFileListContent = $TodayInetFileListContent | Where-Object { $_ -notlike "*-------*" }
-                [array] $Res = @()
-
-                foreach ($Line in $TodayInetFileListContent) {
             
-            
-                    [datetime] $LineDate = Get-Date $line.Substring(0, 18)
-                    $Line = $line.Substring(23, ($line.Length - 23))
-                    $Array1 = $Line.split(":")
-            
-                    if ($Array1[0].contains("Получен файл")) {
-                        $Operation = "Receive"
-                    }
-                    ElseIf ($Array1[0].contains("Отправлен файл")) {
-                        $Operation = "Send"
-                    }
-                    Else {
-                        $Operation = "Unknown"
-                    }
-
-                    $Id = ($Array1[0].trim()).split(" ")[0]
-
-                    $Array2 = ($Array1[1].trim()).split(" ")
-                    $FileName = $Array2[0]
-                    $Size = $Array2[1].remove(0, 1)
-
-                    $PSO = [PSCustomObject]@{
-                        Date      = $LineDate
-                        Operation = $Operation
-                        Id        = $Id
-                        FileName  = $FileName
-                        Size      = $Size
-                    }
-                    $Res += $PSO
-                }    
-                Return $Res
-            }
-
             if ( $LogCutDate ) {
                 $Date = $LogCutDate
             }
@@ -162,10 +138,10 @@ switch ($Service.ToUpper()) {
             
 
             if (Test-path "$ConsUserDataFolder\$ConsErrorFile") {
-                $Content = Get-Content -Path "$ConsUserDataFolder\$ConsErrorFile" -Encoding Default -Tail $ReadLastLines
+                $Content = Get-Content -Path "$ConsUserDataFolder\$ConsErrorFile" -Encoding Default -Tail $ReadLastLines  
                 [array] $TodayErrorContent  = @()
                 [array] $TodayErrorContent1 = @()
-                foreach ($line in $Content) {
+                foreach ($line in $Content) { 
                     if ($line.Length -gt 18) {
                         try {
                             [datetime] $ProbablyDate = Get-Date $line.Substring(0, 18) 
@@ -178,7 +154,6 @@ switch ($Service.ToUpper()) {
                             $TodayErrorContent += $line
                         }
                     }
-
                 }
                 $NewLine = ""
                 foreach ($line in $TodayErrorContent){
@@ -211,20 +186,26 @@ switch ($Service.ToUpper()) {
            
             if (Test-Path "$ConsUserDataFolder\$ConsInetFileList") {
                 $Content = Get-Content -Path "$ConsUserDataFolder\$ConsInetFileList" -Encoding Default -Tail $ReadLastLines
+                [string] $LastLine = $Content | Select-Object -Last 1
+                if ($lastLine.Contains("Средняя скорость скачивания файлов с сервера ИП")) {
+                    $Res.AvgDlSpeed = ($LastLine.Split("-"))[1].Trim()  
+                }
                 [array] $TodayInetFileListContent = @()
                 foreach ($line in $Content) {
-                    if ($line.Length -gt 18) {
-                        try {
-                            [datetime] $ProbablyDate = Get-Date  $line.Substring(0, 18) 
+                    if ($line -notlike "*------*"){
+                        if ($line.Length -gt 18) {
+                            try {
+                                [datetime] $ProbablyDate = Get-Date  $line.Substring(0, 18) 
+                            }
+                            Catch {
+                            }
                         }
-                        Catch {
-                        }
-                    }
-                    if ($ProbablyDate) {
-                        if ($ProbablyDate -ge $Date) {
-                            $TodayInetFileListContent += $line
-                        }
-                    }                    
+                        if ($ProbablyDate) {
+                            if ($ProbablyDate -ge $Date) {
+                                $TodayInetFileListContent += $line
+                            }
+                        }   
+                    }                 
                 }
                 #Convert-InetFileListContentToCSV 
                 $res.ConsInetFileList = $TodayInetFileListContent
@@ -237,30 +218,31 @@ switch ($Service.ToUpper()) {
                 $Content = Get-Content -Path "$ConsUserDataFolder\$ConsInetFile" -Encoding Default -Tail $ReadLastLines
                 [array] $TodayInetFileContent = @()
                 foreach ($line in $Content) {
-                    if ($line.Length -gt 18) {
-                        try {
-                            [datetime] $ProbablyDate = Get-Date  $line.Substring(0, 18) 
+                    if ($line -notlike "*------*") {
+                        if ($line.Length -gt 18) {
+                            try {
+                                [datetime] $ProbablyDate = Get-Date  $line.Substring(0, 18) 
+                            }
+                            Catch {
+                            }
                         }
-                        Catch {
+                        if ($ProbablyDate) {
+                            if ($ProbablyDate -ge $Date) {
+                                $TodayInetFileContent += $line
+                            }
                         }
-                    }
-                    if ($ProbablyDate) {
-                        if ($ProbablyDate -ge $Date) {
-                            $TodayInetFileContent += $line
-                        }
-                    }
-                    
+                    }                    
                 }
                 $res.ConsInetFile = $TodayInetFileContent
             }
             Else {
                         Add-ToLog -Message "Error file ["$ConsUserDataFolder\$ConsInetFile"] not found!" -logFilePath $ScriptLogFilePath -Display -Status "Error" -Level ($ParentLevel + 1)
             } 
-            
+            $Res.LogBuffer = $Global:LogBuffer
             Return $Res
         }        
         
-        Add-ToLog -Message "Starting statistic." -logFilePath $ScriptLogFilePath -Display -Status "Info" -Level ($ParentLevel + 1)
+        Add-ToLog -Message "Starting statistic on computer [$RemoteComputer]." -logFilePath $ScriptLogFilePath -Display -Status "Info" -Level ($ParentLevel + 1)
         $Res = Invoke-PSScriptBlock -ScriptBlock $Scriptblock -Computer $RemoteComputer -ImportLocalModule "AlexkUtils"
         if ($res) {
             $Data = $res.ConsErrorFile
@@ -280,8 +262,13 @@ switch ($Service.ToUpper()) {
                 foreach ($item in $Data) {
                     Add-ToLog -Message $item -logFilePath $ScriptLogFilePath -Display -Status "Info" -Level ($ParentLevel + 1)
                 }                
-            }         
-            if ($Res.LogBuffer){
+            }  
+            $Data = $res.AvgDlSpeed
+            if ($Data) {     
+                Add-ToLog -Message "Average download speed [$Data]." -logFilePath $ScriptLogFilePath -Display -Status "Info" -Level ($ParentLevel + 1)
+            }      
+
+            if (@($Res.LogBuffer).Count){
                 foreach ($item in $Res.LogBuffer) {
                     Add-ToLog @item
                 }
